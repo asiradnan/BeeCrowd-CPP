6c6e298f2d355732af9efec1d3e8d0cc5842631e
AsirAdnan <106885381+AsirAdnan@users.noreply.github.com>
2022-08-01 23:14:16 +0600

    Create 1006.cpp

---
+++ 1006.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double a,b,c;
+    cin>>a>>b>>C;
+    a*=2;
+    b*=3;
+    c*=5;
+    cout<<fixed<<setprecision(1)<<"MEDIA = "<<(a+b+c)/10<<endl;
+    return 0;
+}
