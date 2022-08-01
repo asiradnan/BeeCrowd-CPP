8128e566164110b07796e397abd1dbf030b1f97b
AsirAdnan <106885381+AsirAdnan@users.noreply.github.com>
2022-08-01 23:40:52 +0600

    Create 1037.cpp

---
+++ 1037.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double a;
+    cin>>a;
+    if(a>=0 && a<=25)
+    {cout<<"Intervalo [0,25]"<<endl;};
+    if(a>25 && a<=50)
+    {cout<<"Intervalo (25,50]"<<endl;};
+    if(a>50 && a<=75)
+    {cout<<"Intervalo (50,75]"<<endl;};
+    if(a>75 && a<=100)
+    {cout<<"Intervalo (75,100]"<<endl;};
+    if (a<0 || a>100){cout<<"Fora de intervalo"<<endl;}
+}
