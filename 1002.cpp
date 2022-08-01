3f95a2c458320741b18a9db64a0541e3424f1549
AsirAdnan <106885381+AsirAdnan@users.noreply.github.com>
2022-08-01 23:08:15 +0600

    Create 1002.cpp

---
+++ 1002.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    double b;
+    cin>>b;
+    cout<<fixed<<setprecision(4)<<"A="<<b*b*3.14159<< endl;
+    return 0;
+}
