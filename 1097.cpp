43f73e90b3fb9fe3e1e3e23a4cf20b0952c840ae
Asir Adnan <106885381+AsirAdnan@users.noreply.github.com>
2022-08-04 23:10:57 +0600

    Create 1097.cpp

---
+++ 1097.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int j=7;
+    for (int i=1;i<=9;i+=2){
+        for (int x=0;x<3;x++){
+            cout<<"I="<<i<<" J="<<j<<endl;
+            j--;
+        }
+        j+=5;
+    }
+}
