c0fbe0f15fd467b8fe25e0fe408e98c2595cc178
Asir Adnan <106885381+AsirAdnan@users.noreply.github.com>
2022-08-04 20:26:22 +0600

    Create 1071.cpp

---
+++ 1071.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d=0;
+    cin>>a>>b;
+    if (a<b){
+        d=a;
+    }
+    else{
+        d=b;
+        b=a;
+    }
+    a=d;
+    a++;
+    while (a<b){
+        if (a%2!=0){
+            c+=a;
+        }
+        a++;
+    }
+    cout<<c<<endl;
+}
