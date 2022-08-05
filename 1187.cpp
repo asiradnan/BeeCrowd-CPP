7b895cc4a0b237416365f8afe8c717b8cab3bee9
Asir Adnan <106885381+AsirAdnan@users.noreply.github.com>
2022-08-06 02:56:50 +0600

    Create 1187.cpp

---
+++ 1187.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int j=0;
+    double c,x[12][12],s=0;
+    string b;
+    cin>>b;
+    for (int i=0;i<12;i++){
+        for (j;j<12;j++){
+            cin>>c;
+            x[i][j]=c;
+        }
+        j=0;
+    }
+    int l=1;
+    for (int k=0;k<=4;k++){
+        while (l<=10-k){
+            s+=x[k][l];
+            l++;
+        }
+        l=k+2;
+    }
+    if (b=="S"){
+        cout<<fixed<<setprecision(1)<<s<<endl;
+    }
+    else{
+        cout<<fixed<<setprecision(1)<<s/30<<endl;
+    }
+}
