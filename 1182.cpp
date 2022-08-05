0c7db126b5e1d933cd5a0854741675bf5376d80b
Asir Adnan <106885381+AsirAdnan@users.noreply.github.com>
2022-08-06 02:29:54 +0600

    Create 1182.cpp

---
+++ 1182.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int a,j=0;
+    double c,x[12][12],s=0;
+    cin>>a;
+    string b;
+    cin>>b;
+    for (int i=0;i<12;i++){
+        for (j;j<12;j++){
+            cin>>c;
+            x[i][j]=c;
+        }
+        j=0;
+    }
+    for (int k=0;k<12;k++){
+        s+=x[k][a];
+    }
+    if (b=="S"){
+        cout<<fixed<<setprecision(1)<<s<<endl;
+    }
+    else{
+        cout<<fixed<<setprecision(1)<<s/12<<endl;
+    }
+}
