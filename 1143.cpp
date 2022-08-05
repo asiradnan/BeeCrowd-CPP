def5b1726974312519e2b4f78dcfd0f71759a8da
Asir Adnan <106885381+AsirAdnan@users.noreply.github.com>
2022-08-05 09:17:48 +0600

    Create 1143.cpp

---
+++ 1143.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,x=1;
+    cin>>a;
+    for (int i=0;i<a;i++){
+        cout<<x<<" "<<x*x<<" "<<x*x*x<<endl;
+        x++;
+    }
+}
