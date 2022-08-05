e9acaceafd6bdffa97d9d584a13c12bb142f67ae
Asir Adnan <106885381+AsirAdnan@users.noreply.github.com>
2022-08-06 02:53:14 +0600

    Create 1186.cpp

---
+++ 1186.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int j=0;
+    double c,x[12][12],s=0;
+    string b;
+    cin>>b;
+    for (int i=0;i<12;i++){
+        for (j;j<12;j++){
+            cin>>c;
+            x[i][j]=c;
+        }
+        j=0;
+    }
+    int l=11;
+    for (int k=1;k<=11;k++){
+        while (l>=12-k){
+            s+=x[k][l];
+            l--;
+        }
+        l=11;
+    }
+    if (b=="S"){
+        cout<<fixed<<setprecision(1)<<s<<endl;
+    }
+    else{
+        cout<<fixed<<setprecision(1)<<s/66<<endl;
+    }
+}
