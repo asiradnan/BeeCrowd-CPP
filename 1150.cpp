2dfff0a203d2cc98fb273409dde09e573a090a84
Asir Adnan <106885381+AsirAdnan@users.noreply.github.com>
2022-08-05 10:07:20 +0600

    Create 1150.cpp

---
+++ 1150.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,c=1;
+    cin>>a;
+    int z=a,b=a;
+    while (z<=a){
+        cin>>z;
+    }
+    while (a<=z){
+            a+=b;
+            c++;
+            b++;
+    }
+    cout<<c<<endl;
+}
