ebf26fa045dcb09c21dad4984dd5c36cfca29d23
Asir Adnan <106885381+AsirAdnan@users.noreply.github.com>
2022-08-05 09:46:56 +0600

    Create 1146.cpp

---
+++ 1146.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    while (true){
+        cin>>a;
+        if (a==0){
+            break;
+        }
+        for (int i=1;i<a;i++){
+            cout<<i<<" ";
+        }
+        cout<<a<<endl;
+    }
+}
