e36cd95be00d5c346d994cfe44e9403187bc7fd2
Asir Adnan <106885381+AsirAdnan@users.noreply.github.com>
2022-08-08 18:21:01 +0600

    Create 2483.cpp

---
+++ 2483.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    cout<<"Feliz nat";
+    for (int i=0;i<a;i++){
+        cout<<"a";
+    }
+    cout<<"l!"<<endl;
+}
