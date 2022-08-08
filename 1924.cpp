d562d56082c80884698ad89b938cf7fe82e05a99
Asir Adnan <106885381+AsirAdnan@users.noreply.github.com>
2022-08-08 18:05:26 +0600

    Create 1924.cpp

---
+++ 1924.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string a;
+    int b;
+    cin>>b;
+    for (int i=0;i<b;i++){
+        cin>>a;
+    }
+    cout<<"Ciencia da Computacao"<<endl;
+}
