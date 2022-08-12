fe731f12f4facdc0cf49a6988cdb06255dd3fe53
Asir Adnan <106885381+AsirAdnan@users.noreply.github.com>
2022-08-12 15:58:55 +0600

    Create 2172.cpp

---
+++ 2172.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long int a,b;
+    while (true){
+        cin>>a>>b;
+        if (a==0&&b==0){
+            break;
+        }
+        else{
+            cout<<a*b<<endl;
+        }
+    }
+}
